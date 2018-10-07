765fb0e806dc8b0b1cb94b28781cf30863312af1
Vidhi Rambhia <vsrambhia@gmail.com>
2018-10-07 12:26:24 +0530

    File segregation done.

---
+++ Unholify/models.py
@@ -0,0 +1,3 @@
+from Unholify import db, login_manager
+from flask_login import UserMixin
+from datetime import datetime
---
+++ Unholify/routes.py
@@ -0,0 +1,19 @@
+import os
+import secrets
+from Unholify import app, db
+from flask import Flask, session, escape, render_template, url_for, flash, redirect, request
+#from Unholify.forms import RegistrationFormParty, RegistrationFormSponser, LoginForm, SelectForm,UpdateAccountFormParty,UpdateAccountFormSponsor, ChatBoxText, RequestForm, InviteForm
+#from Unholify.models import PartyUser, SponsorUser, User, Conversing, Conversation
+import hashlib #for SHA512
+from flask_login import login_user, current_user, logout_user, login_required
+from sqlalchemy.orm import Session
+from math import sqrt
+#from googlemaps import Client as GoogleMaps
+import requests
+from geopy.geocoders import Nominatim
+from sqlalchemy import or_ , and_
+
+@app.route("/")
+@app.route("/home")
+def home():
+    return render_template('home.html')
