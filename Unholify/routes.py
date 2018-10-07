e1a4e392aaf5f7c148a9a7e58003a20b1b57970a
nidheekamble <kamble.nidhee@gmail.com>
2018-10-08 01:05:10 +0530

    aligned changes initial pages

--- Unholify/routes.py
+++ Unholify/routes.py
@@ -111,9 +111,9 @@ def stresslevel():
         current_user.stress_level=form.stress_level.data
         db.session.commit()
         if current_user.stress_level > 5 :
-            return redirect('severeHelp.html',title='We are with you',form=form)
+            return render_template('severeHelp.html',title='We are with you',form=form)
         elif current_user.stress_level > 0 :
-            return redirect('ModerateHelp.html',title='We are with you',form=form)
+            return render_template('ModerateHelp.html',title='We are with you',form=form)
         else:
             flash('You have found your way to a stress and alcohol free life','success')
     return render_template('Stress.html',title='How are you feeling today',form=form)
